36578b73cdd02e5829aa330e0ded2e18f35382ea
JayeshHarod <155717521+JayeshHarod@users.noreply.github.com>
2024-01-05 00:58:31 -0800

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = []; 
+    const pairs = { 
+        "(": ")", 
+        "[": "]", 
+        "{": "}", 
+    }; 
+  
+    for (let char of s) { 
+        if (pairs[char]) { 
+            stack.push(char); 
+        } else if ( 
+            char === ")" || 
+            char === "]" || 
+            char === "}"
+        ) { 
+            if ( 
+                pairs[stack.pop()] !== 
+                char 
+            ) { 
+                return false; 
+            } 
+        } 
+    } 
+  
+    return stack.length === 0; 
 };
 
 module.exports = { isValid };
